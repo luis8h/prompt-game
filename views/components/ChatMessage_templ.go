41c5506ab64c8b3a921e3a2d372e1cb49ff153b2
Luis Schmidmeister <vzbls@t-online.de>
2024-11-07 20:45:26 +0100

    changed ui

--- views/components/ChatMessage_templ.go
+++ views/components/ChatMessage_templ.go
@@ -59,7 +59,7 @@ func ChatMessage(message Message) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><div class=\"max-w-xs p-3 rounded-lg bg-blue-100 text-blue-900\"><p class=\"text-sm\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"><div class=\"p-3 rounded-lg bg-blue-100 text-blue-900\" style=\"max-width: 80%\"><p class=\"text-sm\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
