16c0ec04bd0e8056022cd284afa882480398ace9
Luis Schmidmeister <vzbls@t-online.de>
2024-12-01 14:04:22 +0100

    added theme colors to tailwind and fixed content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,28 @@ const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'views/*.templ',
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "1rem",
-        mobile: "2rem",
-        tablet: "4rem",
-        desktop: "5rem",
-      },
+    content: [
+        'views/**/*.templ',
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+                mobile: "2rem",
+                tablet: "4rem",
+                desktop: "5rem",
+            },
+        },
+        extend: {
+            colors: {
+                primary: "#001c2a",
+                secondary: "#2f4757",
+                highlight: "#cdf6ff",
+            }
+        },
     },
-    extend: {
-      colors: {
-        primary: colors.blue,
-        secondary: colors.yellow,
-        neutral: colors.gray,
-      }
-    },
-  },
-  plugins: [
-  ]
+    plugins: [
+    ]
 }
 
