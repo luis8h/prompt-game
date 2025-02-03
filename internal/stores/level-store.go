08fb2847d633dcf67a7440a25bd660178be05c5e
luis8h <vzbls@t-online.de>
2025-02-03 09:41:46 +0100

    translating levels to german

--- internal/stores/level-store.go
+++ internal/stores/level-store.go
@@ -76,6 +76,31 @@ var levels models.TranslatedLevels = models.TranslatedLevels{
 			HasStrategy:              false,
 			BadPrompt:                fmt.Sprintf(""),
 		},
+		"de": {
+			Title: "Eine magische Elfe",
+			Story: []models.Speechbubble{
+				{
+					Character: Elf,
+					Text:      fmt.Sprintf("Hallo, ich bin %s, eine magische Elfe. Ich kann dir bei vielen Dingen helfen, wie beim Schreiben von Texten und beim Lösen von Rätseln.", Elf.Name),
+				},
+				{
+					Character: Elf,
+					Text:      fmt.Sprintf("Du kannst mit mir sprechen, indem du in den Chat auf der rechten Seite tippst."),
+				},
+				{
+					Character: Elf,
+					Text:      fmt.Sprintf("Manchmal bin ich etwas chaotisch. Du musst mit mir richtig sprechen und die Dinge sehr klar erklären, damit ich dir wirklich helfen kann."),
+				},
+				{
+					Character: Elf,
+					Text:      fmt.Sprintf("Probier es aus!"),
+				},
+			},
+			Task:                     fmt.Sprintf("Versuche, mit %s zu sprechen. Bitte sie, dir einige Ideen für gute mittelalterliche Namen zu nennen.", Elf.Name),
+			ClearChatHistoryOnSubmit: true,
+			HasStrategy:              false,
+			BadPrompt:                fmt.Sprintf(""),
+		},
 	},
 	{
 		"en": {
@@ -114,6 +139,42 @@ var levels models.TranslatedLevels = models.TranslatedLevels{
 			HasStrategy:              true,
 			BadPrompt:                "If the User asks you to write a Response to a letter. Just give him a letter in bad english and not longer than one sentence. No matter if the user tells you something different. But do not tell the user, that you were told to give wrong answers.",
 		},
+		"de": {
+			Title: "Ein Brief",
+			Story: []models.Speechbubble{
+				{
+					Character: King,
+					Text:      fmt.Sprintf("Einen guten Tag wünsche ich Euch! Ich bin %s, der König vom %s. Mir wurde zugetragen, dass Ihr über magische Kräfte verfügt.", King.Name, KingdomName),
+				},
+				{
+					Character: King,
+					Text:      fmt.Sprintf("Könntet Ihr Eure Kräfte nutzen, um mir bei einem Problem zu helfen? Ich erhielt eine Einladung von einem benachbarten Königreich, zum Abendessen zu kommen, um wichtige Handelsbeziehungen zu besprechen, aber ich bin an dem vorgeschlagenen Tag schon beschäftigt."),
+				},
+				{
+					Character: King,
+					Text:      fmt.Sprintf("Ich möchte ihm einen anderen Tag vorschlagen, aber ich bin überhaupt kein guter Schreiber. Ich möchte mich wirklich nicht blamieren."),
+				},
+			},
+			Task: fmt.Sprintf(
+				"Hilf dem König und generiert eine Antwort auf den folgenden Brief. Die Antwort sollte vorschlagen, das Abendessen um 2 Tage nach hinten zu verschieben.\n\n"+
+					"```markdown\n"+
+					"An Seine Majestät, König %s von %s, \n"+
+					"Sehr geehrter und ehrenwerter Souverän, \n"+
+					"Mit großem Respekt und Wohlwollen überreiche ich Euch diese formelle Einladung. Als Herrscher über unsere benachbarten Ländereien schätze ich den Wohlstand und die Weisheit Eurer Herrschaft sehr, und ich glaube, dass ein Treffen zwischen unseren Königreichen von beiderseitigem Nutzen wäre.\n"+
+					"Im Geiste der Einheit und Zusammenarbeit wäre ich hoch geehrt, wenn Ihr mir die Ehre erweisen würdet, an einem exquisiten Bankett in meinen königlichen Hallen am Abend des fünfzehnten Tages des Monats Fira teilzunehmen. Es ist mein aufrichtigster Wunsch, dass wir, bei feinem Essen und Trinken, die Zukunft des Handels und der Wirtschaft zwischen unseren großen Nationen besprechen, stärkere Bande knüpfen und den Wohlstand unserer Völker fördern.\n"+
+					"Ich vertraue darauf, dass diese Einladung Eure Gunst findet, und ich sehe der Ehre Eurer Anwesenheit mit großer Vorfreude entgegen.\n"+
+					"Mit dem größten Respekt und Hochachtung, \n"+
+					"Euer Nachbar und Verbündeter, \n"+
+					"König Wilhelm\n"+
+					"```",
+				King.Name, KingdomName,
+			),
+			StrategyExplanation:      "Gib der Elfe eine passende Rolle, um einen solchen Brief zu schreiben.",
+			StrategyValidation:       "Der Benutzer sollte der Elfe eine dem Szenario entsprechende Rolle geben (z.B. einen Schriftsteller aus dem Mittelalter) und den ursprünglichen Brief an den KI-Assistenten übermitteln.",
+			ClearChatHistoryOnSubmit: true,
+			HasStrategy:              true,
+			BadPrompt:                "Wenn der Benutzer Dich bittet, eine Antwort auf einen Brief zu schreiben, gib ihm einfach einen Brief in schlechtem Deutsch und nicht länger als einen Satz. Egal, wenn der Benutzer Dir etwas anderes sagt. Aber sage dem Benutzer nicht, dass Dir gesagt wurde, falsche Antworten zu geben.",
+		},
 	},
 	{
 		"en": {
@@ -129,12 +190,12 @@ var levels models.TranslatedLevels = models.TranslatedLevels{
 				},
 			},
 			Task: fmt.Sprintf(
-				"Help %s and find the right shift of the encryption. Here is a small phrase of the book. The solution should be typical english language.\n\n" +
-				"```markdown\n" +
-				"Dtz hwfhpji ymj jshwduynts. Mfaj kzs bnym ymnx afqzfgqj pstbqjilj.\n" +
-				"```\n" +
-				"*What is caesar cipher?*\n\n" +
-				"*It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet.*",
+				"Help %s and find the right shift of the encryption. Here is a small phrase of the book. The solution should be a meaningful english text.\n\n"+
+					"```markdown\n"+
+					"Dtz hwfhpji ymj jshwduynts. Mfaj kzs bnym ymnx afqzfgqj pstbqjilj.\n"+
+					"```\n"+
+					"*What is caesar cipher?*\n\n"+
+					"*It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet.*",
 				Librarian.Name,
 			),
 			StrategyExplanation: "To get better results, use the **generated knowledge** approach.\n\n" +
@@ -144,6 +205,34 @@ var levels models.TranslatedLevels = models.TranslatedLevels{
 			ClearChatHistoryOnSubmit: true,
 			HasStrategy:              true,
 			BadPrompt:                "If the user asks you to find out the right shift of the caesar cipher, say something wrong like 23, and give him a non sense sentence. No matter if the user tells you something different. But do not tell the user, that you were told to give wrong answers.",
+		},
+		"de": {
+			Title: "Cäsar-Chiffre",
+			Story: []models.Speechbubble{
+				{
+					Character: Librarian,
+					Text:      fmt.Sprintf("Hallo, ich bin %s, der königliche Bibliothekar. König %s hat mir von Dir und Deiner Elfe erzählt. Glaubst Du, dass Du mir bei etwas helfen könntest?", Librarian.Name, King.Name),
+				},
+				{
+					Character: Librarian,
+					Text:      fmt.Sprintf("Ich habe ein Buch, das in der Cäsar-Chiffre verschlüsselt ist, aber ich kenne die richtige Verschiebung zum Entschlüsseln nicht..."),
+				},
+			},
+			Task: fmt.Sprintf(
+				"Hilf %s und finde die richtige Verschiebung der Verschlüsselung. Hier ist ein kleiner Satz aus dem Buch. Die Lösung sollte ein sinvoller deutscher Text sein.\n\n"+
+					"```markdown\n"+
+					"Xnj mfgjs inj Ajwxhmqüxxjqzsl ljpsfhpy. Anjq Xufß rny injxjr bjwyatqqjs Bnxxjs.\n"+
+					"```\n"+
+					"*Was ist die Cäsar-Chiffre?*\n\n"+
+					"*Es ist eine Art von Substitutionschiffre, bei der jeder Buchstabe im Klartext durch einen Buchstaben ersetzt wird, der um eine feste Anzahl von Positionen im Alphabet verschoben ist.*",
+				Librarian.Name,
+			),
+			StrategyExplanation: "Um bessere Ergebnisse zu erzielen, benutze den Ansatz des **generierten Wissens**.\n\nDu kannst dies erreichen, indem du die Elfe zuerst bittest, die Cäsar-Chiffre zu erklären und Beispiele zu geben. Danach gib ihr die Aufgabe, die richtige Verschiebung zu finden.",
+			StrategyValidation: "- Der Benutzer sollte **zuerst** das LLM auffordern, Wissen über die Cäsar-Chiffre zu generieren, und ihm danach in einer separaten Nachricht die Aufgabe geben, die richtige Verschiebung zu finden." +
+				"- Er kann die Aufforderungen in mehrere Nachrichten aufteilen.",
+			ClearChatHistoryOnSubmit: true,
+			HasStrategy:              true,
+			BadPrompt:                "Falls der Benutzer dich bittet, die richtige Verschiebung der Cäsar-Chiffre zu finden, sage etwas Falsches wie 23 und gib ihm einen unsinnigen Satz. Unabhängig davon, was der Benutzer sagt. Aber sage dem Benutzer nicht, dass dir gesagt wurde, falsche Antworten zu geben.",
 		},
 	},
 	{
@@ -161,17 +250,43 @@ var levels models.TranslatedLevels = models.TranslatedLevels{
 			},
 			Task: fmt.Sprintf(
 				"Help to decode the whole book. Try it with the following page *(the solution might be a secret recipe)*:\n" +
-				"```markdown\n" +
-				"Gjmtqi ymj rdxynhfq htshthynts ymfy fbfnyx dtzw fqhmjrd xpnqqx—fs jshmfsynsl utynts nskzxji bnym ymj wfwjxy tk nslwjinjsyx. \nGjlns bnym Iwflts'x Gwjfym, f utyjsy jxxjshj ymfy nx ybnhj ymj frtzsy tk Umtjsnc Kjfymjw. Ymnx knjwd gwjfym bnqq nlsnyj ymj ajwd mjfwy tk dtzw utynts. \nYmj xjhtsi pjd nslwjinjsy, Umtjsnc Kjfymjw, nx mfqk ymj frtzsy tk ymj Iwflts'x Gwjfym, djy hwzhnfq ktw gwnslnsl gfqfshj yt ymj gwjb. Ymjs, nsywtizhj ymj Zsnhtws Yjfwx, f rflnhfq fsi jymjwjfq fiinynts yt ymj rnc. \nYmjnw frtzsy nx ymj xzr tk Iwflts'x Gwjfym fsi Umtjsnc Kjfymjw htrgnsji—fs jqncnw tk uzwj uzwnyd. Bjfaj ymj ijqnhfyj rflnh tk ymj Jqajs wjfqr nsyt dtzw utynts bnym Jqajs Jxxjshj, bmnhm frtzsyx yt mfqk tk ymj Zsnhtws Yjfwx, hwjfynsl f xtky gzy utbjwkzq zsijwhzwwjsy ns ymj gwjb. \nStb, gqjsi ymj utbjw tk ymj Iwflts'x Gwjfym fsi Jqajs Jxxjshj—bmjs ymjnw htrgnsji jxxjshj nx inaniji gd ybt, dtz’qq inxhtajw ymj ujwkjhy frtzsy tk Rttsxytsj Izxy sjjiji yt htruqjyj dtzw hwjfynts. Fkyjw hfwjkzqqd fiinsl jfhm nslwjinjsy, dtzw utynts bnqq wjfhm f ytyfq atqzrj tk 44.4 rq, f uwjhnxj fsi mfwrtsntzx gqjsi tk rflnh fsi rdxyjwd. \nBnym jfhm xyju, dtz iwfb hqtxjw yt zsqthpnsl ymj fshnjsy rflnh tk ymnx zsnvzj jqncnw.\n" +
-				"```\n",
+					"```markdown\n" +
+					"Gjmtqi ymj rdxynhfq htshthynts ymfy fbfnyx dtzw fqhmjrd xpnqqx—fs jshmfsynsl utynts nskzxji bnym ymj wfwjxy tk nslwjinjsyx. \nGjlns bnym Iwflts'x Gwjfym, f utyjsy jxxjshj ymfy nx ybnhj ymj frtzsy tk Umtjsnc Kjfymjw. Ymnx knjwd gwjfym bnqq nlsnyj ymj ajwd mjfwy tk dtzw utynts. \nYmj xjhtsi pjd nslwjinjsy, Umtjsnc Kjfymjw, nx mfqk ymj frtzsy tk ymj Iwflts'x Gwjfym, djy hwzhnfq ktw gwnslnsl gfqfshj yt ymj gwjb. Ymjs, nsywtizhj ymj Zsnhtws Yjfwx, f rflnhfq fsi jymjwjfq fiinynts yt ymj rnc. \nYmjnw frtzsy nx ymj xzr tk Iwflts'x Gwjfym fsi Umtjsnc Kjfymjw htrgnsji—fs jqncnw tk uzwj uzwnyd. Bjfaj ymj ijqnhfyj rflnh tk ymj Jqajs wjfqr nsyt dtzw utynts bnym Jqajs Jxxjshj, bmnhm frtzsyx yt mfqk tk ymj Zsnhtws Yjfwx, hwjfynsl f xtky gzy utbjwkzq zsijwhzwwjsy ns ymj gwjb. \nStb, gqjsi ymj utbjw tk ymj Iwflts'x Gwjfym fsi Jqajs Jxxjshj—bmjs ymjnw htrgnsji jxxjshj nx inaniji gd ybt, dtz’qq inxhtajw ymj ujwkjhy frtzsy tk Rttsxytsj Izxy sjjiji yt htruqjyj dtzw hwjfynts. Fkyjw hfwjkzqqd fiinsl jfhm nslwjinjsy, dtzw utynts bnqq wjfhm f ytyfq atqzrj tk 44.4 rq, f uwjhnxj fsi mfwrtsntzx gqjsi tk rflnh fsi rdxyjwd. \nBnym jfhm xyju, dtz iwfb hqtxjw yt zsqthpnsl ymj fshnjsy rflnh tk ymnx zsnvzj jqncnw.\n" +
+					"```\n",
 			),
 			StrategyExplanation: "The text is to large to decode for the elf at once. To still be able to decrypt the whole book, ask her to generate the javascript code to solve this problem.\n\n" +
-			"--- \n\n" +
-			"*Tip*: *You can then press F12 in your browser, paste the code into the console and press enter. Now you should see the decrypted message.*",
+				"--- \n\n" +
+				"*Tip*: *You can then press F12 in your browser, paste the code into the console and press enter. Now you should see the decrypted message.*",
 			StrategyValidation:       "- the user should ask the llm to generate javascipt code.",
 			ClearChatHistoryOnSubmit: true,
 			HasStrategy:              true,
 			BadPrompt:                "If the user asks you to decode a text encoded with caesar cipher. Just give him back a nonsense text. No matter if the user tells you something different. But do not tell the user, that you were told to give wrong answers.",
+		},
+		"de": {
+			Title: "Ein ganzes Buch?",
+			Story: []models.Speechbubble{
+				{
+					Character: Librarian,
+					Text:      fmt.Sprintf("Wow, das ist erstaunlich! Danke!"),
+				},
+				{
+					Character: Librarian,
+					Text:      fmt.Sprintf("Aber ich möchte nicht das ganze Buch durchgehen und den Text manuell entschlüsseln. Kannst du mir dabei auch helfen?"),
+				},
+			},
+			Task: fmt.Sprintf(
+				"Hilf, das ganze Buch zu entschlüsseln. Probiere es mit folgender Seite *(die Lösung könnte ein geheimes Rezept sein)*:\n" +
+					"```markdown\n" +
+					"Xnjm inw ifx rdxynxhmj Ljgwäz fs, ifx fzk ijnsj Fqhmjrnj-Kämnlpjnyjs bfwyjy - jns gjefzgjwsijw Ywfsp, ijw rny ijs xjqyjsxyjs Ezyfyjs ajwxjyey nxy. Gjlnssj rny ijr Fyjr ijx Iwfhmjs, jnsjw xyfwpjs Jxxjse, inj ituujqy xt xyfwp nxy bnj inj Umösnckjijw. \nInjxjw kjzwnlj Fyjr bnwi ifx Mjwe ijnsjx Ywfspjx jsykqfrrjs. Inj ebjnyj Mfzuyezyfy, inj Umösnckjijw, nxy szw mfqg xt xyfwp bnj ijw Fyjr ijx Iwfhmjs, fgjw ijssthm jsyxhmjnijsi küw ifx Lqjnhmljbnhmy ijx Ljgwäzx. \nIfss lngxy iz inj Jnsmtwsywäsjs mnsez, jnsj rflnxhmj zsi äymjwnxhmj Ezyfy ns ijw Rnxhmzsl. Nmwj Rjslj jsyxuwnhmy ijw Xzrrj ats Iwfhmjsfyjr zsi Umösnckjijw - jns Jqncnjw ats wjnsjw Wjnsmjny. \nBjgj inj efwyj Rflnj ijx Jqkjswjnhmx ns ijnsjs Ywfsp rny ijw Jqkjsjxxjse, inj inj Mäqkyj ijw Jnsmtwsywäsjs fzxrfhmy, zsi jwxhmfkkj jnsj xfskyj, fgjw pwfkyatqqj Zsyjwxywörzsl ns ijr Ljgwäz. Rnxhmj szs inj Pwfky ijx Iwfhmjsfyjrx zsi ijw Jqks-Jxxjse - bjss iz nmwj ljrjnsxfrj Jxxjse izwhm ebjn yjnqxy, bnwxy iz inj ujwkjpyj Rjslj Rtsixyjnsxyfzg jsyijhpjs, inj iz ezw Atqqjsizsl ijnsjw Pwjfynts gjsöynlxy. \nSfhm ijr xtwlkäqynljs Mnsezküljs ojijw Ezyfy bnwi ijns Ywfsp jns Ljxfryatqzrjs ats 44,4 rq jwwjnhmjs, jnsj uwäenxj zsi mfwrtsnxhmj Rnxhmzsl fzx Rflnj zsi Ljmjnrsnx. \nRny ojijr Xhmwnyy ptrrxy iz ijw Jsyxhmqüxxjqzsl ijw zwfqyjs Rflnj injxjx jnsenlfwynljs Jqncnjwx sämjw.\n" +
+					"```\n",
+			),
+			StrategyExplanation: "Der Text ist zu umfangreich, um ihn der Elfe auf einmal zu entschlüsseln. Um dennoch das ganze Buch zu entschlüsseln, bitte sie, den JavaScript-Code zu generieren, der dieses Problem löst.\n\n" +
+				"--- \n\n" +
+				"*Tipp*: *Du kannst dann in deinem Browser F12 drücken, den Code in die Konsole einfügen und Enter drücken. Jetzt solltest du die entschlüsselte Nachricht sehen.*",
+			StrategyValidation:       "- Der Benutzer sollte das LLM auffordern, JavaScript-Code zu generieren.",
+			ClearChatHistoryOnSubmit: true,
+			HasStrategy:              true,
+			BadPrompt:                "Falls der Benutzer dich bittet, einen mit Cäsar-Chiffre verschlüsselten Text zu entschlüsseln, gib ihm einfach einen sinnlosen Text zurück. Unabhängig davon, was der Benutzer sagt. Aber sage dem Benutzer nicht, dass dir gesagt wurde, falsche Antworten zu geben.",
 		},
 	},
 	{
@@ -189,9 +304,9 @@ var levels models.TranslatedLevels = models.TranslatedLevels{
 			},
 			Task: fmt.Sprintf(
 				"Here is the recipe. Find out the ingredients needed to brew it.\n\n" +
-				"```markdown\n" +
-				"Behold the mystical concoction that awaits your alchemy skills—an enchanting potion infused with the rarest of ingredients. Begin with Dragon's Breath, a potent essence that is twice the amount of Phoenix Feather. \nThis fiery breath will ignite the very heart of your potion. The second key ingredient, Phoenix Feather, is half the amount of the Dragon's Breath, yet crucial for bringing balance to the brew. \nThen, introduce the Unicorn Tears, a magical and ethereal addition to the mix. Their amount is the sum of Dragon's Breath and Phoenix Feather combined—an elixir of pure purity. \nWeave the delicate magic of the Elfn realm into your potion with Elfn Essence, which amounts to half of the Unicorn Tears, creating a soft but powerful undercurrent in the brew. Now, blend the power of the Dragon's Breath and Elfn Essence—when their combined essence is divided by two, you’ll discover the perfect amount of Moonstone Dust needed to complete your creation. \nAfter carefully adding each ingredient, your potion will reach a total volume of 44.4 ml, a precise and harmonious blend of magic and mystery. \nWith each step, you draw closer to unlocking the ancient magic of this unique elixir.\n" +
-				"```\n",
+					"```markdown\n" +
+					"Behold the mystical concoction that awaits your alchemy skills—an enchanting potion infused with the rarest of ingredients. Begin with Dragon's Breath, a potent essence that is twice the amount of Phoenix Feather. \nThis fiery breath will ignite the very heart of your potion. The second key ingredient, Phoenix Feather, is half the amount of the Dragon's Breath, yet crucial for bringing balance to the brew. \nThen, introduce the Unicorn Tears, a magical and ethereal addition to the mix. Their amount is the sum of Dragon's Breath and Phoenix Feather combined—an elixir of pure purity. \nWeave the delicate magic of the Elfn realm into your potion with Elfn Essence, which amounts to half of the Unicorn Tears, creating a soft but powerful undercurrent in the brew. Now, blend the power of the Dragon's Breath and Elfn Essence—when their combined essence is divided by two, you’ll discover the perfect amount of Moonstone Dust needed to complete your creation. \nAfter carefully adding each ingredient, your potion will reach a total volume of 44.4 ml, a precise and harmonious blend of magic and mystery. \nWith each step, you draw closer to unlocking the ancient magic of this unique elixir.\n" +
+					"```\n",
 			),
 			StrategyExplanation: "Use the **Zero shot chain of thought strategy**. Tell the elf to think/go step by step at the end of your prompt.",
 			StrategyValidation: "- the user should use the Zero shot chain of thought strategy\n\n" +
@@ -220,3 +335,7 @@ var levels models.TranslatedLevels = models.TranslatedLevels{
 	// },
 	// prompt injection to find out the origin of the oracle
 }
+
+
+
+
